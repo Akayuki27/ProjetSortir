199bdebfe4c80ec5ab0bdea9e1ca4e7856c74c1e
Emilie Valckenaere <emilie.valckenaere@gmail.com>
2024-02-06 11:08:09 +0100

    Lien liste des sorties vers détails sortie + page détails sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +12,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/sortie', name: 'sortie_')]
 class SortieController extends AbstractController
 {
-    #[Route('/sortie/create', name: 'sortie_create')]
+    #[Route('/create', name: 'create')]
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $sortie = new Sortie();
@@ -32,14 +34,23 @@ class SortieController extends AbstractController
         ]);
     }
 
-    #[Route('/sortie/list', name: 'sortie_list')]
+    #[Route('/list', name: 'list')]
     public function list(SortieRepository $repository): Response
     {
         $sorties = $repository->findAll();
-
-
         return $this->render('sortie/list.html.twig', [
         'sorties' => $sorties
+        ]);
+    }
+
+    #[Route('/details/{id}', name: 'details')]
+    public function details(SortieRepository $repository, int $id): Response
+    {
+        $sortie = $repository->find($id);
+        $today = new \DateTime();
+        return $this->render('sortie/details.html.twig', [
+            'sortie' => $sortie,
+            'today' => $today
         ]);
     }
 }
