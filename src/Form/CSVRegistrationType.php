dd187615cb3f10aed5c0667a9cca5c68396e3398
Shataigne <152864877+Shataigne@users.noreply.github.com>
2024-02-07 17:27:25 +0100

    ajout condition fichiers
    pour ajout multiple, bonne soirée !!

--- src/Form/CSVRegistrationType.php
+++ src/Form/CSVRegistrationType.php
@@ -9,14 +9,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CSVRegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('csv', FileType::class,
-                ['label' => 'Fichier CSV'])
+            ->add('csv', FileType::class,[
+                'label' => 'Fichier CSV',
+                'constraints' => [new NotBlank(message: "Fichier obligatoire"),
+                    new File([
+                        'mimeTypes' => ['text/csv'],
+                        'mimeTypesMessage' => 'Veuillez télécharger un fichier CSV valide.',
+                    ]),
+                ],
+            ])
             ->add('estRattacheA', EntityType::class,[
                     'class' => Campus::class,
                     'choice_label' => 'nom',
