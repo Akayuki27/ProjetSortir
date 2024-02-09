d9d824658ead1f8aa36214449b1d0e873cf757b3
Emilie Valckenaere <emilie.valckenaere@gmail.com>
2024-02-09 15:02:04 +0100

    Correction js + format portable ok
    script modifé dans redirect.js et ajout dans list.html.twig de sortie selon la taille de l'écran detectée

--- public/script/redirect.js
+++ public/script/redirect.js
@@ -1,11 +1,13 @@
 // Fonction pour détecter la largeur de l'écran
 function detectScreenSize() {
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
+    let screen = false;
     // Redirection en fonction de la largeur de l'écran
     if (width < 768) { // Exemple : rediriger si la largeur est inférieure à 768px
-        window.location.href = '{{ path("mes_sorties") }}'; // Remplacez 'page_petit_ecran' par l'URL de la page de redirection pour les petits écrans
+       // window.location.href = '{{ path("mes_sorties") }}'; // Remplacez 'page_petit_ecran' par l'URL de la page de redirection pour les petits écrans
+        screen = true;
     }
+    return screen;
 }
 
 // Appel de la fonction au chargement de la page
