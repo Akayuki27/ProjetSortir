4a05162610cdb0819c2a8ca1837d975a1c052bd9
BenjaminQuemeneur <benjamin.quemeneur2023@campus-eni.fr>
2024-02-12 15:22:22 +0100

    J'ai pas d'idée de nom

--- src/Form/GroupType.php
+++ src/Form/GroupType.php
@@ -6,6 +6,7 @@ use App\Entity\Group;
 use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +15,17 @@ class GroupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Nom de votre groupe',
+                ],
+            ])
             ->add('participant', EntityType::class, [
                 'class' => Participant::class,
-'choice_label' => 'pseudo',
-'multiple' => true,
+                'choice_label' => 'pseudo',
+                'multiple' => true,
                 'expanded'=> true,
+                'label'=> 'Participants :'
             ])
         ;
     }
